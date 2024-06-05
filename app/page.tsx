9ea820ce2ac5b2a22d2eb1f574c1884ca9e89a42
jmatondo <j_matondo@hotmail.com>
2024-06-05 16:22:29 +0200

    Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Convergence
+      <Button>Convergence</Button>
     </div>
   );
 }

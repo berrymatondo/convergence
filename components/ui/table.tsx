1feb47310113b6a98ff2a0c9389ca26c588b1582
jmatondo <j_matondo@hotmail.com>
2024-06-22 15:28:32 +0200

    po

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-2 md:px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 px-1 md:px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
@@ -88,7 +88,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 md:p-4 align-middle [&:has([role=checkbox])]:pr-0",
+      "p-1 md:p-4 align-middle [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
